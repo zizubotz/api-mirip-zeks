ba89853e205d53fda1e0913b32d650c3f233da8a
zeeone <82263175+zeeoneofc@users.noreply.github.com>
2021-12-02 14:37:18 +0800

    update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -31,7 +31,7 @@ xhr.send();
 // Visitor
 function getvisitor(){
     var xhr = new XMLHttpRequest();
-    var url = 'https://api.countapi.xyz/hit/janbot-api.herokuapp.com/visits';
+    var url = 'https://api.countapi.xyz/hit/api-alphabot.herokuapp.com/visits';
     xhr.onloadend = function(){
     data = JSON.parse(this.responseText);
     document.getElementById("visits").textContent = data.value
