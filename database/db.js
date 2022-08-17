728f6b03e8dbffda230d7c87a029982571375e2e
ZizuPriaMahal <109475144+zizubotz@users.noreply.github.com>
2022-08-17 18:41:51 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://kuhong-api.herokuapp.com';
+var url = 'https://apizizu.herokuapp.com';
 try {
-if(url == 'https://kuhong-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://apizizu.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
